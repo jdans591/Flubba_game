4a1cf1482d35e38b5f28ed9d69583a3afe9c771e
Harvey <hren243@EN-283894.UoA.auckland.ac.nz>
2015-09-24 18:39:53 +1200

    Initial project
    The basic unity project which can be used as the basis for other work.

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	Rigidbody2D rb;
+	public KeyCode jump;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float xspeed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+		float yspeed = rb.velocity.y;
+
+		// jumping
+		if (Input.GetKeyDown(jump)) {
+			yspeed = 10;
+		}
+
+		// update player velocity
+		rb.velocity = new Vector2 (500 * xspeed, yspeed);
+	}
+}
