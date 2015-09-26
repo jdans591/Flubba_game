80860db970dc7fdcfd482e3ee2a9a2137705178c
Harvey <hren243@EN-283899.UoA.auckland.ac.nz>
2015-09-26 18:17:48 +1200

    Added Ray-Tracing drawing
    Rays are now drawn from the player object

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,26 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(PlayerPhysics))]
 public class PlayerInput : MonoBehaviour {
-	Rigidbody2D rb;
-	public KeyCode jump;
+    PlayerPhysics controller;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
-	}
-
-	// Update is called once per frame
-	void Update () {
-		float xspeed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-		float yspeed = rb.velocity.y;
-
-		// jumping
-		if (Input.GetKeyDown(jump)) {
-			yspeed = 10;
-		}
-
-        // update player velocity
-		rb.velocity = new Vector2 (500 * xspeed, yspeed);
-	}
+    void Start()
+    {
+        controller = GetComponent<PlayerPhysics>();
+    }
 }
