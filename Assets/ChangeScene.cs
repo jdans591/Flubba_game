1aafee76246f3a5408adbfb960d18551f9d178c9
ElliotWhiley <elliot.w@hotmail.com>
2015-10-01 16:05:29 +1300

    added ChangeScene commenting - changes focus from main menu to game level

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ChangeScene : MonoBehaviour {
 	
-	// Update is called once per frame
+	// Changes current focus to specified scene (find scene by number under file -> Build Settings)
 	public void ChangeToScene (int sceneToChangeTo) {
         Application.LoadLevel(sceneToChangeTo);
 	}
