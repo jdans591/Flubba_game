4f89c8f80cc0a91ca4c65db0f5d74208d4c164ee
playspout <playspout@gmail.com>
2015-10-01 19:28:44 +1300

    Smoothen wall jumping by reducing the airborne acceleration. Prevents the object from falling off the wall with left/right button. Increased the wall jumping force to allow object to reach further walls, and to prevent the scenario where an object can continuously jump onto the same wall over and over again. Generalized the air charge system to float to allow for more customizability and extension.
    Implemented a functionality for the down button; it can be pressed while on a wall to drop off the wall.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,9 +9,10 @@ using System.Collections;
 [RequireComponent(typeof(PlayerPhysics))]
 public class PlayerInput : MonoBehaviour 
 {
+
     public float jumpHeight = 4;
     public float timeToJumpApex = .6f;
-    float accelerationTimeAirborne = .2f;
+    float accelerationTimeAirborne = .35f;
     float accelerationTimeGrounded = .1f;
     float moveSpeed = 6;
     float wallSlidingSpeed = 1;
@@ -22,10 +23,12 @@ public class PlayerInput : MonoBehaviour
     float velocityXSmoothing;
 
     bool wallJump;
-    bool airCharge;
+    int airCharge;
 
     PlayerPhysics controller;
 
+
+    /**Initialisation */
     void Start() {
 		//The controller is what handles our movement in the game world
         controller = GetComponent<PlayerPhysics>();
@@ -37,7 +40,7 @@ public class PlayerInput : MonoBehaviour
 
         //Player ability setup
         wallJump = false;
-        airCharge = true;
+        airCharge = 0;
     }
 
 	/**
@@ -47,18 +50,20 @@ public class PlayerInput : MonoBehaviour
 	 */
 
     void Update() {
+
         Debug.Log(Input.GetAxisRaw("Horizontal"));
-		//Vertical collision detection
-        if (controller.collisions.above || controller.collisions.below) {
+
+		//Vertical collision detection. If the player touches the ground or ceiling set vertical velocity to zero.
+        if (TouchingGround() || TouchingCeiling()) {
             velocity.y = 0;
             //If player lands, reset airCharge
-            if (controller.collisions.below) {
-                airCharge = true;
+            if (TouchingGround()) {
+                airCharge = 1;
             }
         }
 
-        //Use side collisions 
-        if (controller.collisions.left || controller.collisions.right) {
+        //Use side collisions. Player will slide down the wall at a constant value. 
+        if (TouchingWall()) {
             if (velocity.y < 0) {
                 velocity.y = -wallSlidingSpeed;
                 wallJump = true;
@@ -71,13 +76,74 @@ public class PlayerInput : MonoBehaviour
 		//Get keyboard input
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-		//Jumping
+        //Ignore left button if the object is on the right wall, and ignore right button if the object is on the left wall. 
+        //Also, if the down button is pressed while the object is on a wall, it will slightly move the object off it 
+        //and drop the object down.
+        if(TouchingWall())
+        {
+            
+            if (TouchingRightWall())
+            {
+                if (input.y == -1)
+                {
+                    velocity.x = -moveSpeed / 100;
+                }
+                if (input.x == -1)
+                {
+                    input.x = 0;
+                }
+
+            }
+            else if (TouchingLeftWall())
+            {
+                if(input.y == -1)
+                {
+                    velocity.x = moveSpeed / 100;
+                }
+                if (input.x == 1)
+                {
+                    input.x = 0;
+                }
+            }
+
+        }
+       
+
+        //When the jump button is pressed.
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {//Simply jump if the object is on the ground. 
+            if(TouchingGround())
+            {
+                velocity.y = jumpVelocity;
+            }//If the object is touching a wall, jump in the opposite direction.
+            else if(TouchingWall())
+            {
+                if(TouchingRightWall())
+                {
+                    velocity.y = jumpVelocity;
+                    velocity.x = -moveSpeed * (float)1.5;
+                } else if(TouchingLeftWall())
+                {
+                    velocity.y = jumpVelocity;
+                    velocity.x = moveSpeed * (float)1.5;
+                }
+            }
+            else if(airCharge == 1)
+            {
+                velocity.y = jumpVelocity;
+                airCharge--;
+            }
+
+        }
+
+        /**
+		//When the jump button is pressed.
         if (Input.GetKeyDown(KeyCode.Space)) {
-            //On ground
+            //If the player is on the ground, start jumping.
             if (controller.collisions.below) {
                 velocity.y = jumpVelocity;
             }
-            //Wall jumping
+            //If the player is currently on a wall.
             else if (wallJump && Mathf.Abs(input.x) == 1) {
                 //Check that the jump direction is the opposite of the wall that is attached to
                 if ((input.x == -1 && controller.collisions.right) || (input.x == 1 && controller.collisions.left)) {
@@ -91,7 +157,7 @@ public class PlayerInput : MonoBehaviour
                 velocity.y = jumpVelocity;
             }
         }
-
+        */
         float targetVelocityX = input.x * moveSpeed;
 		//Player accelerates towards top speed in the direction specified by input
         velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
@@ -100,5 +166,81 @@ public class PlayerInput : MonoBehaviour
 
 		//The controller is given a veloity to move the player by
         controller.Move(velocity * Time.deltaTime);
+    }
+
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////
+    /** Helper funcitons */
+    /////////////////////////////////
+
+
+    /**Helper function to check if the object is touching the ground or not. */
+    bool TouchingGround()
+    {
+        if(controller.collisions.below)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    /**Helper function to check if the object is touching either the left or the right side walls.  */
+    bool TouchingWall()
+    {
+        if(controller.collisions.left || controller.collisions.right)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    /**Helper function to check if the object is touching the right hand side wall.  */
+    bool TouchingRightWall()
+    {
+        if(controller.collisions.right)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    /**Helper function to check if the object is touching the left hand side wall.  */
+    bool TouchingLeftWall()
+    {
+        if(controller.collisions.left)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+
+    }
+
+    /**Helper function to check if the object is touching the ceiling or not.  */
+    bool TouchingCeiling()
+    {
+        if(controller.collisions.above)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 }
