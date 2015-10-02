aaf7c3de3ab77fafb736be87d68b1900d38c492b
playspout <playspout@gmail.com>
2015-10-03 03:34:56 +1300

    Added a timer system, and some checkpoints template. Added finish level checkpoint template where the user returns to the main menu (other people can add the intermission screen etc).

---
+++ Assets/Scripts/TimeControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeControl : MonoBehaviour {
+
+    public Text text;
+    public float time;
+
+    private float minute;
+    private float second;
+
+	// Use this for initialization
+	void Start () {
+        text.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //FixedUpdate is called once every fixed interval
+    void FixedUpdate()
+    {
+        time = Time.timeSinceLevelLoad;
+        second = time;
+        if(second >= 60)
+        {
+            
+            minute = Mathf.Floor(time / 60);
+            second = time % 60;
+        }
+        if(time < 60)
+        {
+            text.text = second.ToString("F2");
+            
+        } 
+        else if(time >= 60)
+        {
+            text.text = minute.ToString("F0") + " " + ":" + " " + second.ToString("F2");
+            
+        }
+        
+    }
+}
