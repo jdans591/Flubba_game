ce70ab20f417f05df4a496bba1827150a36a90dc
AndyC <ac_boy94@hotmail.com>
2015-10-03 18:02:01 +1300

    Utilize level manager with check points

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject startPoint;
+    public GameObject currentCheckpoint;
+    public GameObject flubba;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(flubba);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player respawned");
+        flubba.transform.position = currentCheckpoint.transform.position;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        RespawnPlayer();
+        Debug.Log("object left the game area");
+
+    }
+}
