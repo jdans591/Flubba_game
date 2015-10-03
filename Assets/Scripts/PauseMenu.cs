0134ee17e75c6a040ee6ea98ebff8ef46faff5b3
NamjunP <namjun95@hotmail.com>
2015-10-03 17:33:18 +1300

    Created pause functionality. Upon pressing esc the game pauses

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			pauseMenuCanvas.SetActive (false);
+			Time.timeScale = 1;
+		}
+			
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+	}
+
+	public void resume(){
+		isPaused = !isPaused;
+	}
+
+	public void backToMainMenu(){
+		Application.LoadLevel("main_menu");
+	}
+}
