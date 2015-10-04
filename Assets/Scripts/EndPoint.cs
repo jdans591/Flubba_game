b60326d97c8ca6ce4043464748e74aa66b6e3d4a
NamjunP <namjun95@hotmail.com>
2015-10-05 02:15:12 +1300

    Integrated prefab-endmenu with level1.

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -3,23 +3,34 @@ using System.Collections;
 
 public class EndPoint : MonoBehaviour {
 
+	public PauseMenu pauseMenu;
+	public EndMenu endMenu;
+	public GameObject endMenuCanvas;
 	public AudioClip[] audioClip;
 	AudioSource audio;
-
+	public bool isPaused;
 
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource> ();
+		isPaused = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isPaused) {
+			Time.timeScale = 0f;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Player") {
 			PlaySound (0);
+			isPaused = true;
+			pauseMenu.disabled = true;
+			StartCoroutine(Example());
+			endMenu.display();
+			endMenuCanvas.SetActive(true);
 		}
 	}
 
@@ -27,5 +38,9 @@ public class EndPoint : MonoBehaviour {
 	{
 		audio.clip = audioClip [clip];
 		audio.Play ();
+	}
+
+	IEnumerator Example() {
+		yield return new WaitForSeconds(30);
 	}
 }
