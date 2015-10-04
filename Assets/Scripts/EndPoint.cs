958ced128af98b37c63aef614361e5674bf1f1a4
NamjunP <namjun95@hotmail.com>
2015-10-05 01:20:40 +1300

    Adjusted player sfx bgm volume added sfx to endpoint of level1

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class EndPoint : MonoBehaviour {
 
+	public AudioClip[] audioClip;
+	AudioSource audio;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		audio = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			PlaySound (0);
+		}
+	}
+
+	void PlaySound(int clip)
+	{
+		audio.clip = audioClip [clip];
+		audio.Play ();
 	}
 }
