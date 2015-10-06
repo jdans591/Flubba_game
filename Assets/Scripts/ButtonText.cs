45546c6d4b38ec5f8583304b8c45784bcf848159
tota965 <tota965@aucklanduni.ac.nz>
2015-10-06 15:51:23 +1300

    Intro screen added with no story

---
+++ Assets/Scripts/ButtonText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class ButtonText : MonoBehaviour {
+
+
+	public Text Btext;
+
+	// Use this for initialization
+	void Start () {
+		Btext.text = "Next";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void lastClick(string msg){
+		Btext.text = msg;
+	}
+}
+
---
+++ Assets/Scripts/Storyboard.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Storyboard : MonoBehaviour {
+
+	public Text text;
+	private int count = 0;
+	ButtonText contButton;
+
+	// Use this for initialization
+	void Start () {
+		setText("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setText(string msg){
+		text.text = msg;
+	}
+
+	public void clicked(){
+		if (count == 0) {
+			setText ("First chapter.");
+			count++;
+		} else if (count == 1) {
+			setText ("Second chapter");
+			count++;
+		} else if (count == 2) {
+			setText ("Third chapter");
+			//contButton.lastClick("Play");
+			count++;
+		} else if (count == 3) {
+			Application.LoadLevel(3);
+		}
+	}
+}
