ad0732b2fa7f305f34ba71fcbe1c958290f6be7a
NamjunP <namjun95@hotmail.com>
2015-10-10 21:26:29 +1300

    Created a database manager which calls http POST method to upload playerscore (time) on level 1.

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DatabaseManager : MonoBehaviour {
+
+    public void PostScore(string level, string name, string time)
+    {
+        string url = "https://microsoft-apiapp72ef49a46b6242d28d294f2cda80c2cf.azurewebsites.net/api/Values";
+        WWWForm form = new WWWForm();
+        form.AddField("level", level);
+        form.AddField("name", name);
+        form.AddField("time", time);
+        WWW www = new WWW(url, form);
+        StartCoroutine(WaitForRequest(www));
+
+        Debug.Log(form.data);
+    }
+
+    public void GetLeaderboard()
+    {
+
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.data);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+}
