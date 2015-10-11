c9f1e6966f5648ab5f31398c62779aee6e3e8fa1
AndyC <ac_boy94@hotmail.com>
2015-10-11 21:33:49 +1300

    Bunsen Burner with interval times when fire is turned on and off

---
+++ Assets/Scripts/BunsenBurnerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunsenBurnerController : MonoBehaviour {
+
+    public GameObject firePrefab;
+    GameObject fireSpawn;
+    GameObject currentFire;
+    public bool fireOn;
+
+    void Awake()
+    {
+        fireSpawn = this.gameObject.transform.GetChild(0).gameObject;
+        fireOn = false;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("OperateFire", 3, 2F);
+    }
+
+    void OperateFire()
+    {
+        if (!fireOn)
+        {
+            currentFire = Instantiate(firePrefab, fireSpawn.transform.position, 
+                transform.rotation) as GameObject;
+            fireOn = true;
+        }
+        else
+        {
+            Destroy(currentFire);
+            fireOn = false;
+        }
+    }
+}
