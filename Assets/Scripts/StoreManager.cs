7c5e2644de472e2b1c4afb513a797ff58410b687
SeongwooJ <deram9090@hotmail.com>
2015-10-17 16:16:28 +1300

    Updated Store Scene file and added StoreManager Script

---
+++ Assets/Scripts/StoreManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreManager : MonoBehaviour {
+
+    public GameObject SkinCanvas1;
+    public GameObject SkinCanvas2;
+    public GameObject SkinCanvas3;
+    public GameObject SkinCanvas4;
+
+    public void ChangeSkinCanvas (int skinNum)
+    {
+        switch (skinNum)
+        {
+            //pressed back button from canvas 1
+            case 1:
+                SkinCanvas1.SetActive(false);
+                SkinCanvas4.SetActive(true);
+                break;
+
+            //pressed next button from canvas 1
+            case 2:
+                SkinCanvas1.SetActive(false);
+                SkinCanvas2.SetActive(true);
+                break;
+
+            //pressed back button from canvas 2
+            case 3:
+                SkinCanvas2.SetActive(false);
+                SkinCanvas1.SetActive(true);
+                break;
+
+            //pressed next button from canvas 2
+            case 4:
+                SkinCanvas2.SetActive(false);
+                SkinCanvas3.SetActive(true);
+                break;
+
+            //pressed back button from canvas 3
+            case 5:
+                SkinCanvas3.SetActive(false);
+                SkinCanvas2.SetActive(true);
+                break;
+
+            //pressed next button from canvas 3
+            case 6:
+                SkinCanvas3.SetActive(false);
+                SkinCanvas4.SetActive(true);
+                break;
+
+            //pressed back button from canvas 4
+            case 7:
+                SkinCanvas4.SetActive(false);
+                SkinCanvas3.SetActive(true);
+                break;
+
+            //pressed next button from canvas 4
+            case 8:
+                SkinCanvas4.SetActive(false);
+                SkinCanvas1.SetActive(true);
+                break;
+        }
+    }
+}
