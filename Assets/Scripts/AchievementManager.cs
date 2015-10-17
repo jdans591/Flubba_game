8ef036bade82c06b915571a883ab53f94ec1c09f
ElliotWhiley <elliot.w@hotmail.com>
2015-10-17 17:26:00 +1300

    added locks + ticks to achievements

---
+++ Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementManager : MonoBehaviour {
+
+	// Add ticks to achievements which have been completed
+	void Start () {
+	
+	}
+}
