55e6ac65721f2ce886903adbde9714ea1dfcfb3a
SeongwooJ <deram9090@hotmail.com>
2015-10-17 16:57:05 +1300

    more functionality to StoreManager script

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StoreManager : MonoBehaviour {
+public class StoreManager : MonoBehaviour
+{
 
+    public GameObject player;
     public GameObject SkinCanvas1;
     public GameObject SkinCanvas2;
     public GameObject SkinCanvas3;
     public GameObject SkinCanvas4;
+    public GameObject RandomeCanvas;
 
-    public void ChangeSkinCanvas (int skinNum)
+    public void ChangeSkinCanvas(int skinNum)
     {
         switch (skinNum)
         {
             //pressed back button from canvas 1
             case 1:
                 SkinCanvas1.SetActive(false);
-                SkinCanvas4.SetActive(true);
+                RandomeCanvas.SetActive(true);
                 break;
 
             //pressed next button from canvas 1
@@ -57,7 +60,46 @@ public class StoreManager : MonoBehaviour {
             //pressed next button from canvas 4
             case 8:
                 SkinCanvas4.SetActive(false);
+                RandomeCanvas.SetActive(true);
+                break;
+
+            //pressed back button from RandomeCanvas
+            case 9:
+                RandomeCanvas.SetActive(false);
+                SkinCanvas4.SetActive(true);
+                break;
+
+            //pressed next button from RandomeCanvas
+            case 10:
+                RandomeCanvas.SetActive(false);
                 SkinCanvas1.SetActive(true);
+                break;
+        }
+    }
+
+    public void applySkin(int skinNum)
+    {
+        switch (skinNum)
+        {
+            case 1:
+                player.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("");
+                break;
+
+            case 2:
+                player.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("");
+                break;
+
+            case 3:
+                player.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("");
+                break;
+
+            case 4:
+                player.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("");
+                break;
+
+            case 5:
+                int num = Random.Range(1, 4);
+                applySkin(num);
                 break;
         }
     }
