b27a285d3e15b52c8ad54be5e9269b9572ed8643
NamjunP <namjun95@hotmail.com>
2015-10-19 16:38:56 +1300

    commenting code

--- Assets/Scripts/Collectables + Time/TimeEntry.cs
+++ Assets/Scripts/Collectables + Time/TimeEntry.cs
@@ -19,22 +19,27 @@ public class TimeEntry : MonoBehaviour {
 	
 	}
 
+    // set the text inside player column
     public void setPlayer(string x)
     {
         Player.text = x;
     }
 
+    // set the tet inside time column
     public void setTIme(string y)
     {
         Time.text = y;
     }
 
+
+    // triggered upon button click. plays the replay of corresponding entry of leaderboard.
     public void playReplay(int currentlevel)
     {
         Debug.Log("timelist guid: " + guid);
 
         ProgressCircle.SetActive(true);
 
+        //make a get request to database to download corresponding replay file.
         string url = "https://microsoft-apiapp72ef49a46b6242d28d294f2cda80c2cf.azurewebsites.net/api/Replays?fileid="+guid;
         WWW www = new WWW(url);
         StartCoroutine(WaitForRequest(www, currentlevel));
